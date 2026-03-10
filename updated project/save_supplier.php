216328f7f2db6e3bf3b10290c6e1591b77402385
23uca029 <23uca029@loyolacollege.edu>
2026-03-10 18:12:07 +0530

    Add files via upload

--- updated project/save_supplier.php
+++ updated project/save_supplier.php
@@ -1,28 +1,37 @@
 <?php
 include 'config.php';
 
-if (isset($_POST['submit'])) {
+if(isset($_POST['supplier_code'])){
 
-    $supplier_code = $_POST['supplier_code'];
-    $supplier_name = $_POST['supplier_name'];
-    $addr1         = $_POST['addr1'];
-    $addr2         = $_POST['addr2'];
-    $addr3         = $_POST['addr3'];
-    $pincode       = $_POST['pincode'];
-    $mobile        = $_POST['mobile'];
+$supplier_code = $_POST['supplier_code'];
+$supplier_name = $_POST['supplier_name'];
+$addr1 = $_POST['addr1'];
+$addr2 = $_POST['addr2'];
+$addr3 = $_POST['addr3'];
+$pincode = $_POST['pincode'];
+$mobile = $_POST['mobile'];
 
-    $sql = "INSERT INTO suppliers
-            (supplier_code, supplier_name, addr1, addr2, addr3, pincode, mobile)
-            VALUES
-            ('$supplier_code', '$supplier_name', '$addr1', '$addr2', '$addr3', '$pincode', '$mobile')";
+$sql = "INSERT INTO suppliers
+(supplier_code,supplier_name,addr1,addr2,addr3,pincode,mobile)
+VALUES
+('$supplier_code','$supplier_name','$addr1','$addr2','$addr3','$pincode','$mobile')";
 
-    if ($conn->query($sql) === TRUE) {
-        echo "<h2>Supplier Details Saved Successfully!</h2>";
-        echo "<a href='supplier.php'>Go Back</a>";
-    } else {
-        echo "Error: " . $conn->error;
-    }
+if($conn->query($sql)==TRUE){
 
-    $conn->close();
+echo "<h2>Supplier Details Saved Successfully!</h2>";
+echo "<a href='view_suppliers.php'>Go Back</a>";
+
+}else{
+
+echo "Database Error: " . $conn->error;
+
 }
+
+}else{
+
+echo "Form not submitted properly.";
+
+}
+
+$conn->close();
 ?>
